a5b2a92269cb6ba8061db22680bb92c6c58a0cac
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2015-08-25 01:36:04 +0200

    authorization promise

--- dist/index.js
+++ dist/index.js
@@ -1,173 +1,16 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var mod;
+var DataSource, Injector, Resource, mod;
 
 mod = angular.module('adminr-datasources', ['ngResource', 'ngStorage']);
 
+Resource = require('./lib/Resource.coffee');
+
+DataSource = require('./lib/DataSource.coffee');
+
+Injector = require('./lib/Injector.coffee');
+
 mod.provider('DataSources', function() {
-  var DataSource, DataSourcesProvider, Resource, _$injector;
-  _$injector = null;
-  Resource = (function() {
-    function Resource(dataSource1, path1, paramDefualts1, actions1, options1) {
-      var i, len, method, methods;
-      this.dataSource = dataSource1;
-      this.path = path1;
-      this.paramDefualts = paramDefualts1;
-      this.actions = actions1;
-      this.options = options1;
-      methods = ['get', 'save', 'query', 'remove', 'delete'];
-      for (i = 0, len = methods.length; i < len; i++) {
-        method = methods[i];
-        this[method] = function() {
-          var params;
-          params = new Array(arguments);
-          params.shift(this.resource);
-          return _$injector.get('$q')((function(_this) {
-            return function(resolve, reject) {
-              return _this.resource().query.apply(params).$promise.then(resolve)["catch"](function(error) {
-                var ref;
-                console.log(_this);
-                if ((ref = error.status) === 401 || ref === 429) {
-                  _this.dataSource.logout();
-                }
-                return reject(error);
-              });
-            };
-          })(this));
-        };
-      }
-    }
-
-    Resource.prototype.resource = function() {
-      if (!this._resource) {
-        this._resource = _$injector.get('$resource')(this.path, this.paramDefaults, this.actions, this.options);
-      }
-      return this._resource;
-    };
-
-    return Resource;
-
-  })();
-  DataSource = (function() {
-    DataSource.prototype.authorizationEndpoint = '/authorize';
-
-    DataSource.prototype.resources = {};
-
-    function DataSource(name1, url1, options1) {
-      this.name = name1;
-      this.url = url1;
-      this.options = options1;
-      this.options = this.options || {};
-    }
-
-    DataSource.prototype.isAuthorized = function() {
-      return this.getAuthorizationToken() !== null;
-    };
-
-    DataSource.prototype._getStorage = function(type) {
-      this._storages = this._storages || {};
-      if (!this._storages[type]) {
-        this._storages[type] = _$injector.get('$' + type + 'Storage');
-      }
-      return this._storages[type];
-    };
-
-    DataSource.prototype.getAuthorizationToken = function() {
-      var key, token;
-      key = this.name + '_auth-token';
-      token = this._getStorage('local')[key] || this._getStorage('session')[key] || null;
-      return token;
-    };
-
-    DataSource.prototype.setAuthorizationToken = function(token, sessionOnly) {
-      var key, storage;
-      if (sessionOnly == null) {
-        sessionOnly = false;
-      }
-      key = this.name + '_auth-token';
-      delete this._getStorage('local')[key];
-      delete this._getStorage('session')[key];
-      if (token) {
-        storage = this._getStorage(sessionOnly ? 'session' : 'local');
-        return storage[key] = token;
-      }
-    };
-
-    DataSource.prototype.addResource = function(name, path, paramDefualts, actions, options) {
-      var headers;
-      if (options == null) {
-        options = {};
-      }
-      headers = {
-        Authorization: (function(_this) {
-          return function() {
-            var token;
-            token = _this.getAuthorizationToken();
-            if (!token) {
-              return null;
-            }
-            return 'Bearer ' + token;
-          };
-        })(this)
-      };
-      actions = {
-        'get': {
-          method: 'GET',
-          headers: headers
-        },
-        'save': {
-          method: 'POST',
-          headers: headers
-        },
-        'query': {
-          method: 'GET',
-          isArray: true,
-          headers: headers
-        },
-        'remove': {
-          method: 'DELETE',
-          headers: headers
-        },
-        'delete': {
-          method: 'DELETE',
-          headers: headers
-        }
-      };
-      return this.resources[name] = new Resource(this, this.url + path, paramDefualts, actions, options);
-    };
-
-    DataSource.prototype.removeResource = function(name) {
-      return delete this.resources[name];
-    };
-
-    DataSource.prototype.getResource = function(name) {
-      return this.resources[name];
-    };
-
-    DataSource.prototype.authorize = function(username, password, sessionOnly) {
-      var $http;
-      if (sessionOnly == null) {
-        sessionOnly = false;
-      }
-      $http = _$injector.get('$http');
-      return $http.post(this.url + this.authorizationEndpoint, {
-        username: username,
-        password: password
-      }).then((function(_this) {
-        return function(response) {
-          return _this.setAuthorizationToken(response.data.token, sessionOnly);
-        };
-      })(this))["catch"](function(error) {
-        return console.log('error', error);
-      });
-    };
-
-    DataSource.prototype.logout = function() {
-      return this.setAuthorizationToken(null);
-    };
-
-    return DataSource;
-
-  })();
+  var DataSourcesProvider;
   DataSourcesProvider = (function() {
     function DataSourcesProvider() {}
 
@@ -191,11 +34,8 @@ mod.provider('DataSources', function() {
       return this.dataSources[name];
     };
 
-    DataSourcesProvider.prototype.$get = function($resource, $q, $injector) {
-      var _$q, _$resource;
-      _$resource = $resource;
-      _$q = $q;
-      _$injector = $injector;
+    DataSourcesProvider.prototype.$get = function($injector) {
+      Injector._$injector = $injector;
       return this;
     };
 
@@ -206,4 +46,195 @@ mod.provider('DataSources', function() {
 });
 
 
-},{}]},{},[1]);
+},{"./lib/DataSource.coffee":2,"./lib/Injector.coffee":3,"./lib/Resource.coffee":4}],2:[function(require,module,exports){
+var DataSource, Injector, Resource;
+
+Resource = require('./Resource.coffee');
+
+Injector = require('./Injector.coffee');
+
+DataSource = (function() {
+  DataSource.prototype.authorizationEndpoint = '/authorize';
+
+  DataSource.prototype.resources = {};
+
+  function DataSource(name1, url, options1) {
+    this.name = name1;
+    this.url = url;
+    this.options = options1;
+    this.options = this.options || {};
+  }
+
+  DataSource.prototype.isAuthorized = function() {
+    return this.getAuthorizationToken() !== null;
+  };
+
+  DataSource.prototype._getStorage = function(type) {
+    this._storages = this._storages || {};
+    if (!this._storages[type]) {
+      this._storages[type] = Injector._$injector.get('$' + type + 'Storage');
+    }
+    return this._storages[type];
+  };
+
+  DataSource.prototype.getAuthorizationToken = function() {
+    var key, token;
+    key = this.name + '_auth-token';
+    token = this._getStorage('local')[key] || this._getStorage('session')[key] || null;
+    return token;
+  };
+
+  DataSource.prototype.setAuthorizationToken = function(token, sessionOnly) {
+    var key, storage;
+    if (sessionOnly == null) {
+      sessionOnly = false;
+    }
+    key = this.name + '_auth-token';
+    delete this._getStorage('local')[key];
+    delete this._getStorage('session')[key];
+    if (token) {
+      storage = this._getStorage(sessionOnly ? 'session' : 'local');
+      return storage[key] = token;
+    }
+  };
+
+  DataSource.prototype.addResource = function(name, path, paramDefualts, actions, options) {
+    var headers;
+    if (options == null) {
+      options = {};
+    }
+    headers = {
+      Authorization: (function(_this) {
+        return function() {
+          var token;
+          token = _this.getAuthorizationToken();
+          if (!token) {
+            return null;
+          }
+          return 'Bearer ' + token;
+        };
+      })(this)
+    };
+    actions = {
+      'get': {
+        method: 'GET',
+        headers: headers
+      },
+      'save': {
+        method: 'POST',
+        headers: headers
+      },
+      'query': {
+        method: 'GET',
+        isArray: true,
+        headers: headers
+      },
+      'remove': {
+        method: 'DELETE',
+        headers: headers
+      },
+      'delete': {
+        method: 'DELETE',
+        headers: headers
+      }
+    };
+    return this.resources[name] = new Resource(this, this.url + path, paramDefualts, actions, options);
+  };
+
+  DataSource.prototype.removeResource = function(name) {
+    return delete this.resources[name];
+  };
+
+  DataSource.prototype.getResource = function(name) {
+    return this.resources[name];
+  };
+
+  DataSource.prototype.authorize = function(username, password, sessionOnly) {
+    var $http;
+    if (sessionOnly == null) {
+      sessionOnly = false;
+    }
+    $http = Injector._$injector.get('$http');
+    return Injector._$injector.get('$q')((function(_this) {
+      return function(resolve, reject) {
+        return $http.post(_this.url + _this.authorizationEndpoint, {
+          username: username,
+          password: password
+        }).then(function(response) {
+          _this.setAuthorizationToken(response.data.token, sessionOnly);
+          return resolve(response);
+        })["catch"](function(error) {
+          console.log('error', error);
+          return reject(error);
+        });
+      };
+    })(this));
+  };
+
+  DataSource.prototype.logout = function() {
+    return this.setAuthorizationToken(null);
+  };
+
+  return DataSource;
+
+})();
+
+module.exports = DataSource;
+
+
+},{"./Injector.coffee":3,"./Resource.coffee":4}],3:[function(require,module,exports){
+module.exports = {
+  _$injector: null
+};
+
+
+},{}],4:[function(require,module,exports){
+var Injector, Resource;
+
+Injector = require('./Injector.coffee');
+
+Resource = (function() {
+  function Resource(dataSource, path, paramDefualts, actions, options) {
+    var i, len, method, methods;
+    this.dataSource = dataSource;
+    this.path = path;
+    this.paramDefualts = paramDefualts;
+    this.actions = actions;
+    this.options = options;
+    methods = ['get', 'save', 'query', 'remove', 'delete'];
+    for (i = 0, len = methods.length; i < len; i++) {
+      method = methods[i];
+      this[method] = function() {
+        var params;
+        params = new Array(arguments);
+        params.shift(this.resource);
+        return Injector._$injector.get('$q')((function(_this) {
+          return function(resolve, reject) {
+            return _this.resource().query.apply(params).$promise.then(resolve)["catch"](function(error) {
+              var ref;
+              if ((ref = error.status) === 401 || ref === 429) {
+                _this.dataSource.logout();
+              }
+              return reject(error);
+            });
+          };
+        })(this));
+      };
+    }
+  }
+
+  Resource.prototype.resource = function() {
+    if (!this._resource) {
+      this._resource = Injector._$injector.get('$resource')(this.path, this.paramDefaults, this.actions, this.options);
+    }
+    return this._resource;
+  };
+
+  return Resource;
+
+})();
+
+module.exports = Resource;
+
+
+},{"./Injector.coffee":3}]},{},[1]);
--- index.coffee
+++ index.coffee
@@ -1,98 +1,11 @@
 mod = angular.module('adminr-datasources',['ngResource','ngStorage'])
 
+Resource = require('./lib/Resource.coffee')
+DataSource = require('./lib/DataSource.coffee')
+
+Injector = require('./lib/Injector.coffee')
+
 mod.provider('DataSources',()->
-
-  _$injector = null
-
-  class Resource
-    constructor:(@dataSource,@path,@paramDefualts,@actions,@options)->
-
-      methods = ['get','save','query','remove','delete']
-
-      for method in methods
-        @[method] = ()->
-          params = new Array(arguments)
-          params.shift(@resource)
-          return _$injector.get('$q')((resolve,reject)=>
-            @resource().query.apply(params).$promise.then(resolve).catch((error)=>
-              console.log(@)
-              if error.status in [401,429]
-                @dataSource.logout()
-              reject(error)
-            )
-          )
-
-    resource:()->
-#      if not _$resource
-#        throw new Error('ngResource is not assigned yet (probably due to manipulation with resource methods in config state)')
-      if not @_resource
-        @_resource = _$injector.get('$resource')(@path,@paramDefaults,@actions,@options)
-      return @_resource
-
-  class DataSource
-    authorizationEndpoint: '/authorize'
-
-    resources: {}
-
-    constructor: (@name,@url,@options)->
-      @options = @options or {}
-
-    isAuthorized:()->
-      return @getAuthorizationToken() isnt null
-
-    _getStorage:(type)->
-      @_storages = @_storages or {}
-      if not @_storages[type]
-        @_storages[type] = _$injector.get('$' + type + 'Storage')
-      return @_storages[type]
-
-    getAuthorizationToken:()->
-      key = @name + '_auth-token'
-      token = @_getStorage('local')[key] or @_getStorage('session')[key] or null
-      return token
-    setAuthorizationToken:(token,sessionOnly = no)->
-      key = @name + '_auth-token'
-      delete @_getStorage('local')[key]
-      delete @_getStorage('session')[key]
-      if token
-        storage = @_getStorage(if sessionOnly then 'session' else 'local')
-        storage[key] = token
-
-    addResource: (name,path,paramDefualts,actions,options = {})->
-      headers = {Authorization:()=>
-        token = @getAuthorizationToken()
-        if not token
-          return null
-        return 'Bearer ' + token
-      }
-
-      actions = {
-        'get':    {method:'GET',headers:headers},
-        'save':   {method:'POST',headers:headers},
-        'query':  {method:'GET', isArray:true,headers:headers},
-        'remove': {method:'DELETE',headers:headers},
-        'delete': {method:'DELETE',headers:headers}
-      }
-      @resources[name] = new Resource(@,@url + path,paramDefualts,actions,options)
-
-    removeResource: (name)->
-      delete @resources[name]
-
-    getResource: (name)->
-      return @resources[name]
-
-    authorize: (username,password,sessionOnly = no)->
-      $http = _$injector.get('$http')
-      $http.post(@url + @authorizationEndpoint,{username:username,password:password}).then((response)=>
-        @setAuthorizationToken(response.data.token,sessionOnly)
-      ).catch((error)->
-        console.log('error',error)
-      )
-
-    logout: ()->
-      @setAuthorizationToken(null)
-
-
 
   class DataSourcesProvider
     dataSources: {}
@@ -110,10 +23,8 @@ mod.provider('DataSources',()->
         name = Object.keys(@dataSources)[0]
       return @dataSources[name]
 
-    $get:($resource,$q,$injector)->
-      _$resource = $resource
-      _$q = $q
-      _$injector = $injector
+    $get:($injector)->
+      Injector._$injector = $injector
       return @
 
   return new DataSourcesProvider()
